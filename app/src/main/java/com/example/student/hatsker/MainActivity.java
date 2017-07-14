f5c3d116358ef28b05503852f16571936c85f24a
JakiroBestSupport <leonkhvatskin@gmail.com>
2017-07-14 14:48:23 +0300

    New Implementation, Needs to be Fixed

--- app/src/main/java/com/example/student/hatsker/MainActivity.java
+++ app/src/main/java/com/example/student/hatsker/MainActivity.java
@@ -1,29 +1,38 @@
 package com.example.student.hatsker;
 
-import android.content.Intent;
+import android.content.ContentResolver;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-
+public class MainActivity extends AppCompatActivity
+{
     boolean isRegistered = false;
     int duration = Toast.LENGTH_SHORT;
+    ExecutorService executorService;
+    MediaPlayer mediaPlayer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         final MainActivity this_=this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        executorService= Executors.newFixedThreadPool(3);
 
 
         Button ServiceOnButton=(Button)findViewById(R.id.toService);
@@ -32,10 +41,24 @@ public class MainActivity extends AppCompatActivity {
 
         final IntentFilter intentFilter=new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
 
+        ContentResolver contentResolver=getContentResolver();
+        Uri songUri= MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+        Cursor songCursor = contentResolver.query(songUri, null, null, null, null);
+        if (songCursor!=null && songCursor.moveToFirst())
+        {
+            do {
+                String songPath=
+            }
+        }
 
-        ServiceOnButton.setOnClickListener(new View.OnClickListener() {
+        mediaPlayer=new MediaPlayer();
+        mediaPlayer.setDataSource();
+
+        ServiceOnButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 if(!isRegistered)
                 {
                     registerReceiver(Res,intentFilter);
@@ -47,46 +70,96 @@ public class MainActivity extends AppCompatActivity {
                 {
                     Toast.makeText(this_, "Receiver is already on", duration).show();
                 }
-
-
-
             }
         });
 
-
         Button ServiceOffButton=(Button)findViewById(R.id.offService);
 
-        ServiceOffButton.setOnClickListener(new View.OnClickListener() {
+        ServiceOffButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                if (isRegistered) {
+            public void onClick(View view)
+            {
+                if (isRegistered)
+                {
                     unregisterReceiver(Res);
                     isRegistered = false;
                     TextView offOn = (TextView)findViewById(R.id.offOn);
                     offOn.setText("Off");
-                } else
+                }
+                else
                     Toast.makeText(this_, "Receiver is already off", duration).show();
+            }
+        });
+
+        final Button getF=(Button)findViewById(R.id.FileGetter);
+
+        getF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                GetFiles(path);
             }
         });
 
         Button getCon = (Button)findViewById(R.id.getContacts);
 
-        getCon.setOnClickListener(new View.OnClickListener() {
+        getCon.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[] {ContactsContract.CommonDataKinds.Phone._ID, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null);
-                startManagingCursor(cursor);
+            public void onClick(View view)
+            {
+                String phoneNumber = null;
+                Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+                String _ID = ContactsContract.Contacts._ID;
+                String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+                String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+                Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+                String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+                String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+
+                ContentResolver contentResolver = getContentResolver();
+                Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
 
                 if (cursor.getCount() > 0)
                 {
                     while (cursor.moveToNext())
                     {
-                        // process them as you want
-                        Log.i("DATA"," ID "+cursor.getString(0)+" NAME "+cursor.getString(1)+" PHONE "+cursor.getString(2));
+                        String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+                        String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
+                        int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
+
+                        if (hasPhoneNumber > 0)
+                        {
+                            Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+
+                            while (phoneCursor.moveToNext())
+                            {
+                                phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                                Log.d ("hfgjg", name + " " + phoneNumber);
+                            }
+                        }
                     }
                 }
             }
         });
-
     }
+
+    public void GetFiles(String DirectoryPath) {
+        File f = new File(DirectoryPath);
+        f.mkdirs();
+        File[] files = f.listFiles();
+        if (files!=null)
+        {
+            for (int i = 0; i < files.length; i++)
+            {
+                Log.d("hfgjg", files[i].getAbsolutePath());
+                if (files[i].isDirectory()) GetFiles(files[i].getAbsolutePath());
+            }
+        }
+    }
+
+    String path = android.os.Environment.getRootDirectory().getAbsolutePath();
 }
--- app/src/main/java/com/example/student/hatsker/SMSreciever.java
+++ app/src/main/java/com/example/student/hatsker/SMSreciever.java
@@ -13,12 +13,11 @@ import android.widget.Toast;
  * Created by student on 05.07.17.
  */
 
-public class SMSreciever extends BroadcastReceiver
-{
+public class SMSreciever extends BroadcastReceiver {
     final SmsManager sms = SmsManager.getDefault();
+
     @Override
-    public void onReceive (Context context, Intent intent)
-    {
+    public void onReceive(Context context, Intent intent) {
         final Bundle bundle = intent.getExtras();
 
         Log.d("bubug", "ama one of them");
@@ -37,66 +36,19 @@ public class SMSreciever extends BroadcastReceiver
                     String senderNum = phoneNumber;
                     String message = currentMessage.getDisplayMessageBody();
 
-                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+                    Log.d("SmsReceiver", "senderNum: " + senderNum + "; message: " + message);
 
 
                     // Show alert
                     int duration = Toast.LENGTH_LONG;
-                    Toast toast = Toast.makeText(context, "senderNum: "+ senderNum + ", message: " + message, duration);
+                    Toast toast = Toast.makeText(context, "senderNum: " + senderNum + ", message: " + message, duration);
                     toast.show();
-                    getContacts();
                 }
 
-                //Описываем метод:
-            public void getContacts() {
 
-                String phoneNumber = null;
-
-                //Связываемся с контактными данными и берем с них значения id контакта, имени контакта и его номера:
-                Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
-                String _ID = ContactsContract.Contacts._ID;
-                String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
-                String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
-
-                Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-                String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
-                String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
-
-
-
-                StringBuffer output = new StringBuffer();
-                ContentResolver contentResolver = getContentResolver();
-                Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
-
-                //Запускаем цикл обработчик для каждого контакта:
-                if (cursor.getCount() > 0) {
-
-                    //Если значение имени и номера контакта больше 0 (то есть они существуют) выбираем
-                    //их значения в приложение привязываем с соответствующие поля "Имя" и "Номер":
-                    while (cursor.moveToNext()) {
-                        String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
-                        String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
-                        int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
-
-                        //Получаем имя:
-                        if (hasPhoneNumber > 0) {
-                            output.append("\n Имя: " + name);
-                            Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null,
-                                    Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
-
-                            //и соответствующий ему номер:
-                            while (phoneCursor.moveToNext()) {
-                                phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
-                                output.append("\n Телефон: " + phoneNumber);
-                            }
-                        }
-                        output.append("\n");
-                    }
-                } // end for loop
-            } // bundle is null
+            }
         } catch (Exception e) {
-            Log.e("SmsReceiver", "Exception smsReceiver" +e);
-
+            Log.d("SmsReceiver", "Exception smsReceiver" + e);
         }
     }
 }
